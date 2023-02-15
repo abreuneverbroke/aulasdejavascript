1cafa23fc31a5d1421820b467e2ea5b0fdcdd72b
abreuneverbroke <125470016+abreuneverbroke@users.noreply.github.com>
2023-02-15 10:32:53 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let numero = 1
+
+{
+    let numero = 2
+    console.log('dentro = ', numero)
+}
+
+console.log('fora = ' , numero)
+
+var numero1 = 1
+
+{
+  var numero1 = 1
+  console.log('dentro1 = ', numero1)
+
+
+}
+console.log('fora1 = ' , numero1)
